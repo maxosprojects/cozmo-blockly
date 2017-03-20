aruco marker rotation fixed

server/aruco.py
@@ -100,7 +100,7 @@ class Aruco(object):
 			if ids[i] <= 3:
 				table[i] = positions[i][0]
 
-		if len(table) == 3:
+		if self._sceneQuat is None and len(table) == 3:
 			# Find 'normal' of the scene plane (table).
 			# The plane is described by 3 points - 3 markers on the table
 			vals = list(table.values())
@@ -117,6 +117,8 @@ class Aruco(object):
 			# Find quaternion between desired 'normal' and the actual 'normal' of the plane
 			desiredUp = [0.0, 0.0, 1.0]
 			self._sceneQuat = myquat.fromUnitVectors(vector.normalize(normal), desiredUp)
+			self._markerQuatWlast = myquat.toWlast(myquat.div(self._sceneQuat, [0, 0, 0.707, 0.707]))
+			# self._markerQuatWlast = myquat.toWlast(self._sceneQuat)
 
 			# Find center of the scene - center of mass of the triangle
 			self._scenePos = (p1 + p2 + p3) / (-3.0)
@@ -140,7 +142,12 @@ class Aruco(object):
 			rot = rotations[i][0]
 			rotM, _ = cv2.Rodrigues(rot)
 			quat = quaternions.mat2quat(rotM)
-			quat = myquat.div(quat, self._sceneQuat)
+			quat = myquat.toWlast(quat)
+			# quat = myquat.div(quat, self._sceneQuat)
+			quat = myquat.div(quat, self._markerQuatWlast)
+			# quat = myquat.mul(quat, [0.707, 0, 0, 0.707])
+			quat = [-quat[1], -quat[2], -quat[0], quat[3]]
+			# quat = quaternions.qmult(quat, self._sceneQuat)
 			# Translate position
 			pos = positions[i][0]
 			# pos = np.dot(self._sceneTransform, np.append(pos, 1))
@@ -151,8 +158,8 @@ class Aruco(object):
 			ret.append(marker.toDict())
 
 		# Add scene center as a marker
-		# marker = ArucoMarker(10, (self._scenePos * 1000).tolist(), list(self._sceneQuat))
-		# ret.append(marker.toDict())
+		marker = ArucoMarker(10, (self._scenePos * 1000).tolist(), list(self._markerQuatWlast))
+		ret.append(marker.toDict())
 
 		if withFrame:
 			return ret, self._prepareFrame(frame, ids, corners, rotations, positions)